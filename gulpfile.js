6e68c5dde56389d25be771e7814c6243e0fc0f69
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2016-05-24 08:47:41 -0500

    basic template works

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    concat = require('gulp-concat');
+    
+gulp.task('default', ['index','comboSass', 'sass', 'watch']);
+
+gulp.task('watch', function(){
+    
+    gulp.watch('server/client/views/index.html', ['index']);
+    gulp.watch('server/client/styles/scss/*.scss', ['comboSass']);
+    gulp.watch('server/client/styles/style.scss', ['sass']);
+    
+});
+
+gulp.task('index', function(){
+   return gulp.src('server/client/views/index.html')
+          .pipe(gulp.dest('server/public/views/')); 
+    
+});
+
+gulp.task('comboSass', function () {
+   return gulp.src('server/client/styles/scss/*.scss')
+          .pipe(concat('style.scss'))
+          .pipe(gulp.dest('server/client/styles/')); 
+});
+
+gulp.task('sass', function (){
+   return sass('server/client/styles/style.scss')
+          .pipe(gulp.dest('server/public/styles/'));
+});
